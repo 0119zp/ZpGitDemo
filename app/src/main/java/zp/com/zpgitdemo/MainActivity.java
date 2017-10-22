08d4ebd633cd2b4b29e686fdb56da8b92cea8be5
zpan <zpzibo0119@163.com>
2017-10-22 08:50:37 +0800

    测试分支，分支1.0.2 -- 方法setView

--- app/src/main/java/zp/com/zpgitdemo/MainActivity.java
+++ app/src/main/java/zp/com/zpgitdemo/MainActivity.java
@@ -3,13 +3,19 @@ package zp.com.zpgitdemo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 分支 1.0.2
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // 首先 测试分支 的使用 -- 1.0.1
     }
+
+    public void setView(){
+        // 在分支 1.0.2 创建的方法
+    }
+
 }
