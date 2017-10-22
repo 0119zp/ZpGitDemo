6399fd8fb851513405eab3a9d767c49b68cbe95d
zpan <zpzibo0119@163.com>
2017-10-22 08:44:07 +0800

    测试分支，建立1.0.1

--- app/src/main/java/zp/com/zpgitdemo/MainActivity.java
+++ app/src/main/java/zp/com/zpgitdemo/MainActivity.java
@@ -9,5 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // 首先 测试分支 的使用 -- 1.0.1
     }
 }
